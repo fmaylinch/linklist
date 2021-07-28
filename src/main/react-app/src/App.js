fb0c1fe2228d5ef43975222e9f3ad3232269cadb
fmaylinch <ferranmaylinch@gmail.com>
2021-07-28 22:33:13 +0200

    React app - display more info in list

--- src/main/react-app/src/App.js
+++ src/main/react-app/src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect, useContext } from "react";
 import { Switch, Route, useHistory } from "react-router-dom";
-
+import { Alert } from '@material-ui/lab';
 import { UserContext } from "./components/UserContext";
 import ItemDetail from "./components/ItemDetail";
 import ItemList from "./components/ItemList";
@@ -12,6 +12,7 @@ export default function App() {
 
   const [selectedItem, setSelectedItem] = useState(null);
   const [items, setItems] = useState(null);
+  const [alert, setAlert] = useState(null);
 
   const history = useHistory();
 
@@ -23,6 +24,7 @@ export default function App() {
   }
 
   async function loadItems() {
+    setAlert({severity:"info", text:"Loading items"});
     const search = { username: user.username }; // TODO: may contain list of tags
     const resp = await myAxios
       .post("items/search", search)
@@ -35,7 +37,10 @@ export default function App() {
     if (!user) {
       history.push("/login");
     } else {
-      loadItems().then(items => setItems(items));
+      loadItems().then(items => {
+        setAlert(null);
+        setItems(items);
+      });
     }
   }, [user, history]);
 
@@ -55,10 +60,8 @@ export default function App() {
             <ItemDetail item={selectedItem} />
           </Route>
           <Route path="/">
-            { items
-              ? <ItemList items={items} openItem={openItem} />
-              : <div>Loading items...</div>
-            }
+            { alert ? <Alert severity={alert.severity}>{alert.text}</Alert> : "" }
+            { items ? <ItemList items={items} openItem={openItem} /> : "" }
           </Route>
         </Switch>
       </div>
--- src/main/react-app/src/components/ItemList.js
+++ src/main/react-app/src/components/ItemList.js
@@ -1,11 +1,41 @@
+import { Avatar, ListItemAvatar } from '@material-ui/core';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { styled } from '@material-ui/core/styles';
 
-export default function ItemList({items, openItem}) {
+const MyAvatar = styled(Avatar)({
+  borderRadius: 0
+});
+
+const MyListItemText = styled(ListItemText)({
+  '& .tags': {
+    color: "#bbbbbb"
+  }
+});
+
+export default function ItemList({items, openItem}) {  
+
+  function itemDetails(item) {
     return (
-          items.map(item => (
-            <div key={item.id} onClick={() => openItem(item)}>
-              {item.title}
-            </div>
-          ))
+      <>
+        <span className="notes">{item.notes}</span>
+        {item.tags.length
+          ? <span className="tags"><br/>#{item.tags.join(" #")}</span>
+          : ""}
+      </>
     );
   }
-  
+
+  return (
+    items.map(item => (
+      <ListItem button key={item.id} onClick={() => openItem(item)}>
+        {/*
+          <ListItemAvatar>
+            <MyAvatar src={item.image} />
+          </ListItemAvatar>
+        */}
+        <MyListItemText primary={item.title} secondary={itemDetails(item)} />
+      </ListItem>
+    ))
+  );
+}
