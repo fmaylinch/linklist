a192ab75299c46fff81e5f95e71a2c272f155523
Ferran Maylinch <ferran@yandex-team.ru>
2021-07-31 19:26:54 +0200

    React app - AppBar with item count

--- src/main/react-app/src/components/ItemList.js
+++ src/main/react-app/src/components/ItemList.js
@@ -1,20 +1,18 @@
-import { TextField } from '@material-ui/core';
-import { useState } from 'react';
+import {AppBar, IconButton, TextField, Toolbar, Typography} from '@material-ui/core';
+import {Add as AddIcon, Settings as SettingsIcon} from '@material-ui/icons';
+import {useState} from 'react';
 import styled from '@emotion/styled';
 
 const ItemRowDiv = styled.div`
   margin: 10px 0;
   border-bottom: 1px #d5d5d5 solid;
-
   & > .title {
     font-weight: 400;
   }
-
   & > .notes {
     font-weight: 300;
     color: #bbbbbb;
   }
-
   & > .tags {
     color: #aaaaaa;
   }
@@ -32,12 +30,27 @@ function ItemRow({item}) {
   );
 }
 
+function getFilteredItems(query, items) {
+
+  if (!query) return items;
+
+  const parts = query.split(/ +/)
+    .map(x => x[0] === "#" ? x + " " : x); // Add space at the end of tag, for exact search
+
+  return items.filter(item => {
+    for (let part of parts) {
+      // Search for all parts
+      if (item.rawContent.indexOf(part) < 0) return false;
+    }
+    return true;
+  });
+}
+
 export default function ItemList({items, openItem}) {
 
   const [query, setQuery] = useState("");
-  //const classes = useStyles();
 
-  function appBar() {
+  function searchBar() {
     return (
       <div>
         <TextField fullWidth label="search" value={query} onChange={e => setQuery(e.target.value)} />
@@ -45,29 +58,43 @@ export default function ItemList({items, openItem}) {
     );
   }
 
-  function itemList() {
-
-    const lowerQuery = query ? query.trim().toLowerCase() : "";
-    const parts = lowerQuery.split(/ +/)
-      .map(x => x[0] === "#" ? x + " " : x); // Add space at the end of tag, for exact search
-
-    const searchedItems = items.filter(item => {
-      for (let part of parts) {
-        // Search for all parts
-        if (item.rawContent.indexOf(part) < 0) return false;
-      }
-      return true;
-    });
-
-    return searchedItems.map(item => (
+  function itemList(filteredItems) {
+    return filteredItems.map(item => (
       <ItemRow item={item} key={item.id} onClick={() => openItem(item)} />
     ));
   }
 
+  const cleanQuery = query ? query.trim().toLowerCase() : "";
+
+  const filteredItems = getFilteredItems(cleanQuery, items);
+
+  const appBarInfo = cleanQuery
+    ? filteredItems.length + " of " + items.length
+    : items.length;
+
   return (
-      <div style={{margin: "0 10px"}}>
-        {appBar()}
-        {itemList()}
-      </div>
+      <>
+        <AppBar position="sticky">
+          <Toolbar>
+            <Typography variant="h6" color="inherit">
+              LinkList
+            </Typography>
+            <Typography color="inherit"
+                style={{fontWeight: "300", fontSize: "14px", marginLeft: "10px", flexGrow: 1}}>
+              {appBarInfo}
+            </Typography>
+            <IconButton color="inherit">
+              <SettingsIcon />
+            </IconButton>
+            <IconButton edge="end" color="inherit">
+              <AddIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <div style={{margin: "0 10px"}}>
+          {searchBar()}
+          {itemList(filteredItems)}
+        </div>
+      </>
   );
 }
