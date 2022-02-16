4b3c9df2674a29bbc18fcadf1835c8c8f5b309a1
Ferran Maylinch <ferran@yandex-team.ru>
2022-02-17 00:11:06 +0100

    Fix default empty tags

--- src/main/java/com/codethen/linklist/metadata/MetadataScraper.java
+++ src/main/java/com/codethen/linklist/metadata/MetadataScraper.java
@@ -2,9 +2,9 @@ package com.codethen.linklist.metadata;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -35,6 +35,7 @@ public class MetadataScraper {
             final Item.ItemBuilder builder = Item.builder()
                     .title(title != null ? title : doc.title())
                     .url(url)
+                    .tags(Collections.emptyList())
                     .image(getMeta(doc, "property", "og:image"))
                     .notes(getMeta(doc, "property", "og:description"))
                     .score(Item.DEFAULT_SCORE);
