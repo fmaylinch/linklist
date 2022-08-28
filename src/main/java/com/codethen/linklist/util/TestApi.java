2c33574325de24a7e8e6a99c034fb782633229a1
Ferran Maylinch <ferran@yandex-team.ru>
2022-08-28 19:36:46 +0200

    Add /test/files

--- src/main/java/com/codethen/linklist/util/TestApi.java
+++ src/main/java/com/codethen/linklist/util/TestApi.java
@@ -1,56 +1,20 @@
 package com.codethen.linklist.util;
 
 import java.io.File;
-import java.io.FileReader;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.annotation.security.PermitAll;
-import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.SecurityContext;
 
-import com.codethen.linklist.db.MongoService;
-import com.codethen.linklist.db.MongoUtil;
-import com.codethen.linklist.db.MongoUtil.CommonFields;
-import com.codethen.linklist.db.MongoUtil.Ops;
-import com.codethen.linklist.items.Item;
-import com.codethen.linklist.items.ItemAdapter;
-import com.codethen.linklist.items.ItemId;
-import com.codethen.linklist.items.ItemsUpload;
-import com.codethen.linklist.items.ItemsUploadResponse;
-import com.codethen.linklist.items.Search;
-import com.codethen.linklist.items.SearchResult;
-import com.codethen.linklist.items.UpdateMany;
-import com.codethen.linklist.permissions.Permission;
-import com.codethen.linklist.permissions.PermissionService;
-import com.codethen.linklist.security.Roles;
-import com.codethen.linklist.users.User;
-import com.codethen.linklist.users.UserService;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.lang.Nullable;
-import com.opencsv.CSVReader;
-import org.bson.Document;
 import org.jboss.logging.Logger;
-import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
-
-import static com.codethen.linklist.db.MongoUtil.doc;
-import static com.codethen.linklist.items.ItemAdapter.byIdAndUserId;
-import static com.codethen.linklist.items.ItemAdapter.byUserId;
-import static com.codethen.linklist.items.ItemAdapter.byUserIdAndTags;
-import static com.codethen.linklist.util.SecurityUtil.getUserId;
 
 @Path("test")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -62,12 +26,34 @@ public class TestApi {
     @GET @Path("message")
     @PermitAll
     public Message message() {
-        return new Message("This is test message #3 from LinkList app");
+        return new Message("Last change: you can use /test/files?path=SOME_PATH");
+    }
+
+    @GET @Path("files")
+    @PermitAll
+    public FolderInfo files(@QueryParam("path") String path) {
+        var file = new File(path);
+        if (!file.exists()) {
+            return new FolderInfo("path doesn't exist: " + path, null);
+        }
+        if (!file.isDirectory()) {
+            return new FolderInfo("path is not a directory: " + path, null);
+        }
+        var files = Arrays.stream(file.listFiles())
+                .map(File::getName)
+                .collect(Collectors.toList());
+        return new FolderInfo("files in directory: " + path, files);
     }
 
     @lombok.Value
     public static class Message {
         String title;
+    }
+
+    @lombok.Value
+    public static class FolderInfo {
+        String message;
+        List<String> files;
     }
 
 }
